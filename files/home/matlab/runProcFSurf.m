462764be8a91889700cd9245142b50fd86860a8e
Hauke Bartsch <HaukeBartsch@gmail.com>
2023-09-04 16:13:59 +0200

    fix: AFNI installation and format double-quote

--- files/home/matlab/runProcFSurf.m
+++ files/home/matlab/runProcFSurf.m
@@ -3,6 +3,8 @@ function [] = runProcFSurf(parms)
 %   run processing on the local machine
 
 s = dir('/home/MMPS/batchdirs/DAL_ABCD_fsurf/*.csh');
+%batchdirs_folder = sprintf('/home/MMPS/batchdirs/DAL_ABCD_fsurf')
+%s = what(batchdirs_folder);
 
 cd('/home/MMPS/batchdirs/DAL_ABCD_fsurf');
 for i=1:length(s),
